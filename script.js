fcf9073c7c5375e847d6918633099c1393aa36ce
somilia <somilia@somia-pc>
2024-04-10 16:04:01 +0200

    add classes for binaryTree

--- script.js
+++ script.js
@@ -1,5 +1,147 @@
 const BITRATES_ALLOCATIONS = [2, 4, 8, 16, 32, 64, 128, 256];
 
+class Node {
+    constructor(code) {
+        this.code = code;
+        this.isAllocated = null;
+        this.isAncestorsFree = new Boolean(true);
+        this.treeLevel = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    length() {
+        return this.code.length;
+    }
+
+    dot(rhs) {
+        // Does the scalar product "."
+        if (this.length() !== rhs.length()) {
+            throw new Error("Les deux codes n'ont pas la même longueur.");
+        }
+
+        let sum = 0;
+        for (let i = 0; i < this.length(); i++) {
+            sum += this.bits[i] * rhs.bits[i];
+        }
+        return sum;
+    }
+
+    isOrthogonal(rhs) {
+        if (this.length() !== rhs.length()) {
+            throw new Error("Les deux codes n'ont pas la même longueur.");
+        }
+
+        if (this.dot(rhs) !== 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    print(node = this.root, level = 0, prefix = '') {
+        if (node !== null) {
+            this.print(node.right, level + 1, 'R➜');
+            console.log('level '+level+' : '+' '.repeat(level * 4) + prefix + node.code);
+            this.print(node.left, level + 1, 'L⬅');
+        }
+    }
+
+    insert(code) {
+        // Create a node, assign his code, and insert this node as the root if there is not root, call the insertNode function otherwise
+        const newNode = new Node(code);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.code < node.code) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    findOSVFbycode(code) {
+        if (node !== null) {
+            if (node.code === code) {
+                return node;
+            } else {
+                this.findFreeOVSF(node.left);
+                this.findFreeOVSF(node.right);
+            }
+        }
+    }
+
+    findFreeOVSF(node) {
+        if (node !== null) {
+            if (node.isAllocated !== null) {
+                return node;
+            } else {
+                this.findFreeOVSF(node.left);
+                this.findFreeOVSF(node.right);
+            }
+        }
+    }
+    
+    calcShortestFreeCode(requestLen) {
+        if (!isPowerOfTwo(requestLen)) {
+            return 0;
+        }
+    
+        const lastNode = tree.codeCount();
+        const lastLevel = Math.log2(lastNode - 1);
+        for (let lv = 2; lv <= lastLevel; lv++) {
+            const codeLen = 1 << lv;
+    
+            if (codeLen < requestLen) {
+                continue;
+            }
+    
+            const beginNode = 1 << lv;
+            const nodeCount = 1 << lv;
+    
+            for (let k = 0; k < nodeCount; k++) {
+                if (tree.nodes[beginNode + k].isFreeCode()) {
+                    return codeLen;
+                }
+            }
+        }
+        return 0;
+    }
+    
+}
+
+const binaryTree = new BinaryTree();
+const osvfCode_tmp = ["0", "1", "00", "11", "01", "10"];
+for (let i = 0; i < osvfCode_tmp.length; i++) {
+    binaryTree.insert(osvfCode_tmp[i]);
+}
+
+
+
+
+
+
+
 class Device {
     // Description: This class represents a device in the network
     constructor(name){
